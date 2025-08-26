7c5f9adfde704ab4d9ce32dab24b47697720d169
Avihhan <109458161+avihhan@users.noreply.github.com>
2025-08-25 23:32:09 -0400

    Delete activate_venv.bat

--- activate_venv.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Activating GSU AI Virtual Environment...
-call venv\Scripts\activate.bat
-echo Virtual environment activated!
-echo You can now run: python document_upload.py
-cmd /k
